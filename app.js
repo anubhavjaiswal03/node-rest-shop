25a45e015916d926f55be95e2ebbff8f3a329b6c
Anubhav Jaiswal <36488000+anubhavjaiswal03@users.noreply.github.com>
2022-09-28 19:07:11 +0530

    CORS ERROR HANDLING

--- app.js
+++ app.js
@@ -5,17 +5,24 @@ const bodyParser = require("body-parser");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const { json } = require("body-parser");
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//CORS ERROR PREVENTION
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
 	);
+	//To Share the options of all API methods
+	if (req.method === "OPTIONS") {
+		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+		return res.status(200), json({});
+	}
 });
 
 //routes which should handle requests
