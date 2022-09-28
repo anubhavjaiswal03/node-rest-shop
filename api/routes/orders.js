dd26663a3a6c8345d4739d65940dc39153cb80ab
Anubhav Jaiswal <36488000+anubhavjaiswal03@users.noreply.github.com>
2022-09-28 19:01:07 +0530

    added body parser

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,28 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
 	res.status(200).json({
-		message: 'Orders were fetched',
+		message: "Orders were fetched",
 	});
 });
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
+	const order = {
+		productId: req.body.productId,
+		quantity: req.body.quantity,
+	};
+
 	res.status(201).json({
-		message: 'Order was created',
+		message: "Order was created",
+		order: order,
 	});
 });
 
-router.get('/:orderId', (req, res, next) => {
+router.get("/:orderId", (req, res, next) => {
 	res.status(200).json({
-		message: 'Order details',
+		message: "Order details",
 		orderId: req.params.orderId,
 	});
 });
 
-router.delete('/:orderId', (req, res, next) => {
+router.delete("/:orderId", (req, res, next) => {
 	res.status(200).json({
-		message: 'Order deleted',
+		message: "Order deleted",
 		orderId: req.params.orderId,
 	});
 });
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,45 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
 	res.status(200).json({
-		message: 'Handling GET request to /products',
+		message: "Handling GET request to /products",
 	});
 });
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
+	const product = {
+		name: req.body.name,
+		price: req.body.price,
+	};
 	res.status(201).json({
-		message: 'Handling POST request to /products',
+		message: "Handling POST request to /products",
+		createdProduct: product,
 	});
 });
 
-router.get('/:productId', (req, res, next) => {
+router.get("/:productId", (req, res, next) => {
 	const id = req.params.productId;
-	if (id == 'special') {
+	if (id == "special") {
 		res.status(200).json({
-			message: 'You discovered the special ID',
+			message: "You discovered the special ID",
 			id: id,
 		});
 	} else {
 		res.status(200).json({
-			message: 'You passed an ID',
+			message: "You passed an ID",
 			id: id,
 		});
 	}
 });
 
-router.patch('/:productId', (req, res, next) => {
+router.patch("/:productId", (req, res, next) => {
 	const id = req.params.productId;
 	res.status(200).json({
-		message: 'Updated product!',
+		message: "Updated product!",
 		id: id,
 	});
 });
 
-router.delete('/:productId', (req, res, next) => {
+router.delete("/:productId", (req, res, next) => {
 	const id = req.params.productId;
 	res.status(200).json({
-		message: 'Deleted product!',
+		message: "Deleted product!",
 		id: id,
 	});
 });
